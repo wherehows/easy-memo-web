1183e60db2523f0a194ca28c8cf1d8f75975e5bb
Younghoo Kim <sensitive.hu@gmail.com>
2024-01-01 00:57:46 +0900

    feat(WritePage): 입력 영역 일부 구현

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -5,10 +5,23 @@ const WritePage = () => {
     <>
       <Header>
         <Header.LeftOption option={{ back: true }} />
-        <Header.MiddleText text="상세 페이지" />
+        <Header.MiddleText text="작성 페이지" />
         <Header.RightOption option={{ save: true }} />
       </Header>
-      <main></main>
+      <main className="flex flex-col grow py-[16px] px-[24px]">
+        <div className="flex flex-col">
+          <label htmlFor="title">제목</label>
+          <input id="title" type="text" maxLength={50} />
+        </div>
+        <div className="flex flex-col grow">
+          <label htmlFor="content">내용</label>
+          <textarea
+            className="h-[100%] resize-none"
+            id="content"
+            maxLength={2000}
+          />
+        </div>
+      </main>
     </>
   );
 };
