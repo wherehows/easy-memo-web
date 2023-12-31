134449a44a1c42cee6afacb3b6ed6169e12032ab
Younghoo Kim <sensitive.hu@gmail.com>
2023-12-31 20:20:56 +0900

    feat(Main): 페이지 내 플로팅 버튼 추가 및 헤더 삽입

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
+import Header from "@/stories/Header";
+import Link from "next/link";
+
 export default function Home() {
-  return <i className="fa-solid fa-circle-chevron-right"></i>;
+  return (
+    <>
+      <Link
+        href="/create"
+        className="absolute rounded-[50%] bg-green-500 w-[50px] h-[50px] center right-[20px] bottom-[20px] height-[20px]"
+      >
+        <i className="fa-solid fa-plus text-white text-[24px]" />
+      </Link>
+      <Header>
+        <Header.LeftOption logo />
+        <Header.MiddleText text="메인 페이지" />
+        <Header.RightOption
+          option={{ search: true, filter: true, menu: true }}
+        />
+      </Header>
+      <main></main>
+    </>
+  );
 }
