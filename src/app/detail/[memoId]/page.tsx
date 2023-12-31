d9159b9401567846be40b9eb83499d4139f165f9
Younghoo Kim <sensitive.hu@gmail.com>
2024-01-01 00:28:34 +0900

    feat(DetailPage): 디테일 페이지 추가

---
+++ src/app/detail/[memoId]/page.tsx
@@ -0,0 +1,22 @@
+import Header from "@/stories/Header";
+
+interface DetailPageProps {
+  params: { memoId: string };
+}
+
+const DetailPage = ({ params: { memoId } }: DetailPageProps) => {
+  return (
+    <>
+      <Header>
+        <Header>
+          <Header.LeftOption option={{ back: true }} />
+          <Header.MiddleText text="상세 페이지" />
+          <Header.RightOption option={{ save: true }} />
+        </Header>
+      </Header>
+      <main></main>
+    </>
+  );
+};
+
+export default DetailPage;
