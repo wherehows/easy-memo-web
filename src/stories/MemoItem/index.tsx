cbc1da732b47d1e6a52280039cd3ee5654aa9906
Younghoo Kim <sensitive.hu@gmail.com>
2024-01-01 00:02:12 +0900

    feat(MemoItem): 컴포넌트 구현

---
+++ src/stories/MemoItem/index.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+interface MemoItemProps {
+  id: number;
+  title: string;
+  date: string;
+}
+
+const MemoItem = ({ id, title, date }: MemoItemProps) => {
+  return (
+    <>
+      <Link href={`/memo/${id}`}>
+        <h2>{title}</h2>
+      </Link>
+      <time dateTime={date} className="text-gray-400">
+        {date}
+      </time>
+    </>
+  );
+};
+
+export default MemoItem;
