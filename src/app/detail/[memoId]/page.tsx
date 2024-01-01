1c3894f4267d06b7e8c891fd90274990627713f4
Younghoo Kim <sensitive.hu@gmail.com>
2024-01-01 19:38:10 +0900

    fix(DetailPage): 타입 에러 수정

--- src/app/detail/[memoId]/page.tsx
+++ src/app/detail/[memoId]/page.tsx
@@ -1,18 +1,35 @@
 "use client";
 
 import Header from "@/stories/Header";
+import { useRouter } from "next/navigation";
 
 interface DetailPageProps {
   params: { memoId: string };
 }
 
 const DetailPage = ({ params: { memoId } }: DetailPageProps) => {
+  const router = useRouter();
+
   return (
     <>
       <Header>
         <Header>
-          <Header.LeftOption option={{ back: true }} />
-          <Header.RightOption option={{ save: true }} />
+          <Header.LeftOption
+            option={{
+              back: {
+                onClick: () => {
+                  router.back();
+                },
+              },
+            }}
+          />
+          <Header.RightOption
+            option={{
+              save: {
+                onClick: () => {},
+              },
+            }}
+          />
         </Header>
       </Header>
       <main></main>
