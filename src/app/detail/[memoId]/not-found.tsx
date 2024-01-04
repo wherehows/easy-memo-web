57c797890a0b71d14c67eba46de563c55c8cd1d2
Younghoo Kim <sensitive.hu@gmail.com>
2024-01-04 18:05:22 +0900

    feat(NotFoundPage): 페이지 구현

---
+++ src/app/detail/[memoId]/not-found.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import Header from "@/stories/Header";
+import { useRouter } from "next/navigation";
+
+const NotFoundPage = () => {
+  const router = useRouter();
+
+  return (
+    <>
+      <Header>
+        <Header.LeftOption
+          option={{
+            back: {
+              onClick: () => {
+                router.replace("/");
+              },
+            },
+          }}
+        />
+      </Header>
+      <main className="main pt-[8px] pb-[16px] px-[24px]">
+        <div className="grow center text-gray-400">
+          존재하지 않는 메모입니다.
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default NotFoundPage;
