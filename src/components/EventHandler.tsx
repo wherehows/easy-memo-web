be29ed8cfe536e865ca5c6c7192201046ddbe38c
Younghoo Kim <sensitive.hu@gmail.com>
2024-01-29 14:15:51 +0900

    chore(EventListener): 디바이스 뒤로가기 핸들링 로직 추가

--- src/components/EventHandler.tsx
+++ src/components/EventHandler.tsx
@@ -1,13 +1,32 @@
 "use client";
 
+import { postMessage } from "@/utils/helpers";
+import { LOCALES } from "@/utils/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 const EventHandler = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+
   useEffect(() => {
     const handleMessage = (event: Event) => {
       try {
-        const data = (event as MessageEvent).data;
-        alert(data);
+        const { type, data } = JSON.parse((event as MessageEvent).data);
+
+        switch (type) {
+          case "BACK_PRESS": {
+            if (LOCALES.some((lang) => `/${lang}` === pathname)) {
+              postMessage("EXIT_APP");
+              break;
+            }
+
+            router.back();
+            break;
+          }
+          default:
+            alert(`There is no handler related to type: ${type} data: ${data}`);
+        }
       } catch (e) {
         alert(`data from rn parsing is failed.`);
         console.error(e);
@@ -27,7 +46,7 @@ const EventHandler = () => {
         document.removeEventListener("message", handleMessage);
       }
     };
-  }, []);
+  }, [pathname, router]);
 
   return null;
 };
