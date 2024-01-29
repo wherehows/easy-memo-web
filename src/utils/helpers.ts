dc8d926f85e49305e940b391031869dffa9f366e
Younghoo Kim <sensitive.hu@gmail.com>
2024-01-29 13:54:52 +0900

    chore(utils): postmessage 함수 수정

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -45,7 +45,7 @@ export const formatTimeDifference = (
   return dayjs().locale(locale).format("YYYY-MM-DD");
 };
 
-export const postMessage = <T>(type: string, data: T) =>
+export const postMessage = <T>(type: string, data?: T) =>
   window.ReactNativeWebView.postMessage(
     JSON.stringify({
       type,
