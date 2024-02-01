affb66006aea4760104a400286eb7fcb299e1f5e
wherehows <sensitive.hu@gmail.com>
2024-02-01 23:48:41 +0900

    chore(MemoItem): dateTime 속성에 값 할당

--- src/components/MemoItem/index.tsx
+++ src/components/MemoItem/index.tsx
@@ -13,7 +13,9 @@ const MemoItem = ({ id, title, date }: MemoItemProps) => {
       <Link href={`/detail/${id}`}>
         <h2>{title}</h2>
       </Link>
-      <time className="text-sm text-gray-400">{date}</time>
+      <time className="text-sm text-gray-400" dateTime={date}>
+        {date}
+      </time>
     </>
   );
 };
